b0019b93abc3033f242663a18a0723fc47b2bab1
SamHurt4400 <142505662+SamHurt4400@users.noreply.github.com>
2024-12-18 09:19:38 -0500

    Pokemon server avances 2

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -20,7 +20,7 @@ exports.createPokemon = async (req,res)=>{
     }
 }
 
-exports.getPokemon = async(req,res)=>{
+exports.getPokemons = async(req,res)=>{
     try{
         const pokemons = await Pokemon.find({})
         res.status(200).json({pokemons})
@@ -30,4 +30,42 @@ exports.getPokemon = async(req,res)=>{
         return res.status(500).json({error})
 
     }
+}
+
+exports.getPokemonByPokemonId = async (req,res)=>{
+    try{
+        const pokemon_id = req.params.pokemon_id;
+        const pokemon = await Pokemon.findOne({"pokemon_id":pokemon_id});
+        res.status(200).json(pokemon)
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({error})
+
+    }
+
+}
+
+exports.catchPokemonByPokemonId=async (req,res)=>{
+    try{
+        const pokemon_id = req.params.pokemon_id;
+        let pokemon = await Pokemon.findOne({"pokemon_id":pokemon_id})
+        if(!pokemon){
+            return res.status(404).json({menssage:"Pokemon not view yet"})
+        }
+        else if(pokemon.catch){
+            return res.status(200).json(pokemon)
+        }
+        else{
+            pokemon=await Pokemon.findOneAndReplace({"pokemon_id":pokemon_id},{
+                pokemon_id:pokemon_id,
+                view: true,
+                catch: true,
+            },{new:true})
+            return res.status(200).json(pokemon)
+        }
+
+  }catch(error){
+        console.log(error)
+        return res.status(500).json({error});
+}
 }
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const{test,createPokemon}= require("../controllers/pokemon")
+const{test,createPokemon, getPokemons,getPokemonByPokemonId,catchPokemonByPokemonId}= require("../controllers/pokemon")
 const router = express.Router();
 
 router.get("/test",test);
 router.post("/",createPokemon)
-
+router.get("/",getPokemons)
+router.get("/pokemonid/:pokemon_id",getPokemonByPokemonId)
+router.put("/catch/:pokemon_id",catchPokemonByPokemonId)
 module.exports =router
